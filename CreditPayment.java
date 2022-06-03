3ca40c863ab643b9723ca6f4337fe624d76a81e0
Solodyankin Konstantin <Solodyankin.k@gmail.com>
2022-06-04 04:38:56 +0500

    update information description

--- CreditPayment.java
+++ CreditPayment.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class CreditPayment {
     public static void main(String[] args) {
-        double annualRate, M, i, T, A;
-        int godCredit, n;
+        double annualRate, monthlyPayment, monthlyAnnualRate, costOfCredit, loanAmount;
+        int yearOfCredit, monthsOfCredit;
 
         final int MONTHS_PER_YEAR = 12;
 
@@ -23,27 +23,28 @@ public class CreditPayment {
         // Сумма кредита
         // Количество лет
 
-        System.out.print("Введите ежегодную процентную ставку, 8,25: ");
+        System.out.print("Введите ежегодную процентную ставку, например 8,25: ");
         annualRate = input.nextDouble();
 
-        System.out.print("Введите сумму кредита, 120000,95: ");
-        A = input.nextDouble();
+        System.out.print("Введите необходимую сумму кредита, например 120000,95: ");
+        loanAmount = input.nextDouble();
 
-        System.out.print("Введите количество лет погашения кредита, 5: ");
-        godCredit = input.nextInt();
+        System.out.print("Введите количество лет погашения кредита, например 5: ");
+        yearOfCredit = input.nextInt();
 
         // Расчёт
 
-        n = godCredit * MONTHS_PER_YEAR;          // срок кредита в месяцах
-        i = annualRate / MONTHS_PER_YEAR / 100;   // ежемесячная процентная ставка
+        monthsOfCredit = yearOfCredit * MONTHS_PER_YEAR;          // срок кредита в месяцах
+        monthlyAnnualRate = annualRate / MONTHS_PER_YEAR / 100;   // ежемесячная процентная ставка в долях процента
+        /* с долей процента честно не понял, на данный момент...  */
 
-        M = (A * i) / (1 - 1 / Math.pow((1 + i),n) );
-        T = M * n;
+        monthlyPayment = (loanAmount * monthlyAnnualRate) / (1 - 1 / Math.pow((1 + monthlyAnnualRate),monthsOfCredit) );    // формула ежемесячного платёжа
+        costOfCredit = monthlyPayment * monthsOfCredit;           // формура стоимость кредита
 
         // Выводимые данные:
         // Ежемесячный платёж
-        System.out.println("Ежемесячный платёж состовляет " + (int)(M * 100) / 100.0 + " рублей.");
+        System.out.println("Ежемесячный платёж состовляет " + (int)(monthlyPayment * 100) / 100.0 + " рублей.");
         // Стоимость кредита
-        System.out.println("Стоимость кредита состовляет " + (int)(T * 100) / 100.0 + " рублей.");
+        System.out.println("Стоимость кредита состовляет " + (int)(costOfCredit * 100) / 100.0 + " рублей.");
     }
 }
