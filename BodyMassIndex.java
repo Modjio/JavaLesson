96f06b29313ad0dec385c696d217ccb0d351ae83
Solodyankin Konstantin <Solodyankin.k@gmail.com>
2022-06-06 17:17:02 +0500

    minor update

--- BodyMassIndex.java
+++ BodyMassIndex.java
@@ -5,30 +5,30 @@ import java.util.Scanner;
 
 public class BodyMassIndex {
     public static void main(String[] args) {
-        int weight, height;
-        double BMI;
+        double bmi, weight, height;
+        //final int SMS_PER_METER = 100;
 
         Scanner input = new Scanner(System.in);
 
         // Вводная информация
         System.out.print("Введите свой вес в килограммах: ");
-        weight = input.nextInt();
+        weight = input.nextDouble();
         System.out.print("Введите свой рост в сантиметрах: ");
-        height = input.nextInt();
+        height = input.nextDouble();
 
         // Решение по формуле BMI = вес / рост в квадрате
-        BMI = weight / Math.pow(height,2) * 10000;
-        //BMI = weight / (height / 100 * height / 100); // эта формула не работает
+        bmi = weight / Math.pow(height,2) * 1E+4;
+        //BMI = weight / (height / SMS_PER_METER * height / SMS_PER_METER); // эта формула не корректно считает
 
         // Выводимая информация
-        System.out.println("Ваш индекс массы тела равен " + (int)(BMI * 100) / 100.0);
+        System.out.println("Ваш индекс массы тела равен " + (int)(bmi * 100) / 100.0);
 
         // Интерпритация и вывод результата
-        if (BMI < 18.5)         // Если ИМТ меньше 18,5
+        if (bmi < 18.5)         // Если ИМТ меньше 18,5
             System.out.println("У Вас недостаточный вес");
-        else if (BMI < 25)      // Если ИМТ не меньше 18,5 и меньше 25
+        else if (bmi < 25)      // Если ИМТ не меньше 18,5 и меньше 25
             System.out.println("ИМТ в пределах нормы");
-        else if (BMI < 30)      // Если ИМТ не меньше 25 и меньше 30
+        else if (bmi < 30)      // Если ИМТ не меньше 25 и меньше 30
             System.out.println("У Вас избыточный вес");
         else                    // Если ИМТ больше или равно 30
             System.out.println("У Вас ожирение");
