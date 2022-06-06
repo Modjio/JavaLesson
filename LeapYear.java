4566f64b8269d7af2f48413c191cb40a8ea06f74
Solodyankin Konstantin <Solodyankin.k@gmail.com>
2022-06-07 00:02:46 +0500

    add file

---
+++ LeapYear.java
@@ -0,0 +1,20 @@
+// Напишите программу, которая определяет, является ли введенный год високосным или нет.
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        int year;
+
+        Scanner input = new Scanner(System.in);
+
+        // Вводная информация
+        System.out.print("Введите число: ");
+        year = input.nextInt();
+
+        // Решение
+
+
+        // Выводимая информация
+    }
+}
