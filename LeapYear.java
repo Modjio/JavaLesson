4de20d02ba58cf450e4bf4a01e3a055a2e1f70c9
Solodyankin Konstantin <Solodyankin.k@gmail.com>
2022-06-07 17:54:44 +0500

    Leap ready

--- LeapYear.java
+++ LeapYear.java
@@ -5,16 +5,20 @@ import java.util.Scanner;
 public class LeapYear {
     public static void main(String[] args) {
         int year;
+        boolean isLeapYear;
 
         Scanner input = new Scanner(System.in);
 
         // Вводная информация
-        System.out.print("Введите число: ");
+        System.out.print("Введите год: ");
         year = input.nextInt();
 
-        // Решение
-
+        // Решение по формуле:
+        // Год считается високосным, если он делится без остатка на 4,
+        // но при этом не делится на 100, кроме случая, когда он делится без остатка на 400.
+        isLeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
 
         // Выводимая информация
+        System.out.println(year + " год високосный? - " + isLeapYear);
     }
 }
